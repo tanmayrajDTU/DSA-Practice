454ee45273f72f9dc600b9f4e786cfa7004f9b36
Tanmay Raj <tr4236@gmail.com>
2026-06-25 12:52:13 +0530

    Add questions and answers

---
+++ Basics/02 - If Else Adult Teen/Solution.cs
@@ -0,0 +1,11 @@
+class Solution {
+    public void IsAdult(int age) {
+        // Your code goes here
+        if(age >= 18){
+            Console.WriteLine("Adult");
+        }
+        else{
+            Console.WriteLine("Teen");
+        }
+    }
+}
